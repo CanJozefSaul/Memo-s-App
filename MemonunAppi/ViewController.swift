3c4df8fd76aaf6e45da134c1f0197d138677b7de
Joseph <CanJozefSaul@cans-macbook.local>
2016-10-29 11:22:20 +0300

    Initial Commit

---
+++ MemonunAppi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemonunAppi
+//
+//  Created by Can Jozef Saul on 10/29/16.
+//  Copyright © 2016 Can Jozef Saul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
